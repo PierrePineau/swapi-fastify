b170103fcf6e0b1a07637f8b56975baa8bc4d90c
PierrePineau <pierrepineau.pro@gmail.com>
2023-11-14 10:37:12 +0100

    Config Swagger

--- index.js
+++ index.js
@@ -1,157 +1,182 @@
-const fastify = require("fastify");
-const { PrismaClient } = require("@prisma/client");
+const fastify = require("fastify")()
+const {PrismaClient} = require("@prisma/client")
 
-const prisma = new PrismaClient();
-const app = fastify({ logger: true });
+const prisma = new PrismaClient()
 
-const configSwagger = require("./plugins/swagger.js");
+const configSwagger = require("./plugins/swagger.js")
+const configSwaggerUI = require("./plugins/swagger-ui.js")
 
-app.register(require("@fastify/swagger"), configSwagger);
+const start = async () => {
+	try {
+		// Enregistre le plugin fastify-swagger
+		await fastify.register(require("@fastify/swagger"), configSwagger)
 
-app.post(`/signup`, async (req, res) => {
-  const { name, email, posts } = req.body;
+		// Enregistre le plugin fastify-swagger-ui
+		await fastify.register(require("@fastify/swagger-ui"), configSwaggerUI)
 
-  const postData = posts
-    ? posts.map((post) => {
-        return { title: post.title, content: post.content || undefined };
-      })
-    : [];
+		// Définis une route avec une fonction de gestion
+		fastify.put(
+			"/some-route/:id",
+			{
+				schema: {
+					// Schéma de la route
+				},
+			},
+			(req, reply) => {
+				// Fonction de gestion de la route
+			}
+		)
 
-  const result = await prisma.user.create({
-    data: {
-      name,
-      email,
-      posts: {
-        create: postData,
-      },
-    },
-  });
-  return result;
-});
+		fastify.post(`/signup`, async (req, res) => {
+			const {name, email, posts} = req.body
 
-app.post(`/post`, async (req, res) => {
-  const { title, content, authorEmail } = req.body;
-  const result = await prisma.post.create({
-    data: {
-      title,
-      content,
-      author: { connect: { email: authorEmail } },
-    },
-  });
-  return result;
-});
+			const postData = posts
+				? posts.map((post) => {
+						return {title: post.title, content: post.content || undefined}
+				  })
+				: []
 
-app.put("/post/:id/views", async (req, res) => {
-  const { id } = req.params;
+			const result = await prisma.user.create({
+				data: {
+					name,
+					email,
+					posts: {
+						create: postData,
+					},
+				},
+			})
+			return result
+		})
 
-  try {
-    const post = await prisma.post.update({
-      where: { id: Number(id) },
-      data: {
-        viewCount: {
-          increment: 1,
-        },
-      },
-    });
+		fastify.post(`/post`, async (req, res) => {
+			const {title, content, authorEmail} = req.body
+			const result = await prisma.post.create({
+				data: {
+					title,
+					content,
+					author: {connect: {email: authorEmail}},
+				},
+			})
+			return result
+		})
 
-    return post;
-  } catch (error) {
-    return { error: `Post with ID ${id} does not exist in the database` };
-  }
-});
+		fastify.put("/post/:id/views", async (req, res) => {
+			const {id} = req.params
 
-app.put("/publish/:id", async (req, res) => {
-  const { id } = req.params;
+			try {
+				const post = await prisma.post.update({
+					where: {id: Number(id)},
+					data: {
+						viewCount: {
+							increment: 1,
+						},
+					},
+				})
 
-  try {
-    const postData = await prisma.post.findUnique({
-      where: { id: Number(id) },
-      select: {
-        published: true,
-      },
-    });
+				return post
+			} catch (error) {
+				return {error: `Post with ID ${id} does not exist in the database`}
+			}
+		})
 
-    const updatedPost = await prisma.post.update({
-      where: { id: Number(id) || undefined },
-      data: { published: !postData.published || undefined },
-    });
-    return updatedPost;
-  } catch (error) {
-    return { error: `Post with ID ${id} does not exist in the database` };
-  }
-});
+		fastify.put("/publish/:id", async (req, res) => {
+			const {id} = req.params
 
-app.delete(`/post/:id`, async (req, res) => {
-  const { id } = req.params;
+			try {
+				const postData = await prisma.post.findUnique({
+					where: {id: Number(id)},
+					select: {
+						published: true,
+					},
+				})
 
-  const post = await prisma.post.delete({
-    where: {
-      id: Number(id),
-    },
-  });
-  return post;
-});
+				const updatedPost = await prisma.post.update({
+					where: {id: Number(id) || undefined},
+					data: {published: !postData.published || undefined},
+				})
+				return updatedPost
+			} catch (error) {
+				return {error: `Post with ID ${id} does not exist in the database`}
+			}
+		})
 
-app.get("/users", async (req, res) => {
-  const users = await prisma.user.findMany();
-  return users;
-});
+		fastify.delete(`/post/:id`, async (req, res) => {
+			const {id} = req.params
 
-app.get("/user/:id/drafts", async (req, res) => {
-  const { id } = req.params;
+			const post = await prisma.post.delete({
+				where: {
+					id: Number(id),
+				},
+			})
+			return post
+		})
 
-  const drafts = await prisma.user
-    .findUnique({
-      where: {
-        id: Number(id),
-      },
-    })
-    .posts({
-      where: { published: false },
-    });
+		fastify.get("/users", async (req, res) => {
+			const users = await prisma.user.findMany()
+			return users
+		})
 
-  return drafts;
-});
+		fastify.get("/user/:id/drafts", async (req, res) => {
+			const {id} = req.params
 
-app.get(`/post/:id`, async (req, res) => {
-  const { id } = req.params;
+			const drafts = await prisma.user
+				.findUnique({
+					where: {
+						id: Number(id),
+					},
+				})
+				.posts({
+					where: {published: false},
+				})
 
-  const post = await prisma.post.findUnique({
-    where: { id: Number(id) },
-  });
-  return post;
-});
+			return drafts
+		})
 
-app.get("/feed", async (req, res) => {
-  const { searchString, skip, take, orderBy } = req.query;
+		fastify.get(`/post/:id`, async (req, res) => {
+			const {id} = req.params
 
-  const or = searchString
-    ? {
-        OR: [
-          { title: { contains: searchString } },
-          { content: { contains: searchString } },
-        ],
-      }
-    : {};
+			const post = await prisma.post.findUnique({
+				where: {id: Number(id)},
+			})
+			return post
+		})
 
-  const posts = await prisma.post.findMany({
-    where: {
-      published: true,
-      ...or,
-    },
-    include: { author: true },
-    take: Number(take) || undefined,
-    skip: Number(skip) || undefined,
-    orderBy: {
-      updatedAt: orderBy || undefined,
-    },
-  });
+		fastify.get("/feed", async (req, res) => {
+			const {searchString, skip, take, orderBy} = req.query
 
-  return posts;
-});
+			const or = searchString
+				? {
+						OR: [{title: {contains: searchString}}, {content: {contains: searchString}}],
+				  }
+				: {}
 
-app.listen({ port: 3000 }, () =>
-  console.log(`
-🚀 Server ready at: http://localhost:3000
-⭐️ See sample requests: https://github.com/prisma/prisma-examples/tree/latest/javascript/rest-fastify#using-the-rest-api`)
-);
+			const posts = await prisma.post.findMany({
+				where: {
+					published: true,
+					...or,
+				},
+				include: {author: true},
+				take: Number(take) || undefined,
+				skip: Number(skip) || undefined,
+				orderBy: {
+					updatedAt: orderBy || undefined,
+				},
+			})
+
+			return posts
+		})
+
+		// Attend que Fastify soit prêt
+		await fastify.ready()
+
+		// Lance le serveur Fastify
+		await fastify.listen({port: 3000})
+		console.log(`Serveur lancé sur ${fastify.server.address().port}`)
+	} catch (err) {
+		console.error(err)
+		process.exit(1)
+	}
+}
+
+// Appelle la fonction principale
+start()
